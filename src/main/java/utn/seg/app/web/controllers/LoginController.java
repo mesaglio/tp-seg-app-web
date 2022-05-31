f4c36500de8c94ef5ec7b1647b6b23325d1ccd99
juan mesaglio <juan.mesaglio@gmail.com>
2022-05-31 12:56:29 -0300

    update controllers

--- src/main/java/utn/seg/app/web/controllers/LoginController.java
+++ src/main/java/utn/seg/app/web/controllers/LoginController.java
@@ -6,17 +6,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import utn.seg.app.web.database.MySQLConnector;
 import utn.seg.app.web.jwt.Jwt;
 import utn.seg.app.web.models.LoginRequest;
+import utn.seg.app.web.models.LoginResponse;
 
-@RestController
+@RestController()
+@CrossOrigin(origins = "http://localhost:3000/")
 public class LoginController {
 
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+    private static final Logger logger = LogManager.getLogger("App");
 
     @Autowired
     MySQLConnector mySQLConnector;
@@ -29,9 +29,9 @@ public class LoginController {
         String password = body.getPassword();
         if (mySQLConnector.isValidUser(body.getEmail(), password)) {
             String token = jwtComponent.createJWT(body.getEmail(), true);
-            Claims claim = jwtComponent.decodeJWT(token);
-            logger.info(claim.get("isAdmin"));
-            return ResponseEntity.status(200).header("TOKEN", token).body("");
+            LoginResponse response = new LoginResponse();
+            response.setToken(token);
+            return ResponseEntity.status(200).body(response);
         }
         return ResponseEntity.status(403).body("");
     }
