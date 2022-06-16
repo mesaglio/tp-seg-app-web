4ca6db37f10d4aa1c31864b75a4f4206ee82ed31
juan mesaglio <juan.mesaglio@gmail.com>
2022-06-16 02:31:06 -0300

    end

--- src/main/java/utn/seg/app/web/MainController.java
+++ src/main/java/utn/seg/app/web/MainController.java
@@ -4,6 +4,7 @@ package utn.seg.app.web;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,9 @@ public class MainController {
 
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
-    @GetMapping("/")//@RequestHeader("X-Api-Version") String apiVersion
-    public String index() {
+    @GetMapping("/")//
+    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
+        logger.info(apiVersion);
         logger.info("Received a request for API version ");
         return "Hello, world!";
     }
